3c6e88e618aeb5546cbf7ab20b603a60e3c011a9
Mr. Perfect <ahikmitra1234@gmail.com>
2025-01-11 18:38:47 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+let allImages = Array.from(document.querySelectorAll('.single_img'));
+let previewSection = document.querySelector('.preview');
+let cross = document.querySelector('.cross');
+let previewImg = document.querySelector('.preview_img');
+let right = document.querySelector('.right');
+let left = document.querySelector('.left');
+let currentIndex = 0;
+
+
+
+allImages.map((el, index)=>{
+    el.addEventListener('click', function(){
+       previewSection.classList.add('preview_active')
+       previewImg.src = allImages[index].src
+       currentIndex = index
+       
+       
+       right.addEventListener('click', function(){
+        currentIndex += 1;
+        if(currentIndex >= allImages.length){
+            currentIndex = 0;
+        }
+        previewImg.src = allImages[currentIndex].src
+           
+       })
+
+
+
+
+
+
+       left.addEventListener('click', function(){
+        currentIndex -= 1;
+        if(currentIndex <= allImages.length ){
+            currentIndex = allImages.length -1;
+        }
+        previewImg.src = allImages[currentIndex].src
+           
+       })
+
+    })
+    
+    cross.addEventListener('click', function(){
+        previewSection.classList.remove('preview_active')
+    })
+    
+    previewSection.addEventListener('click', function(e){
+        if(e.target.classList.contains('preview_active')){
+           previewSection.classList.remove('preview_active')
+       }
+    })
+})
